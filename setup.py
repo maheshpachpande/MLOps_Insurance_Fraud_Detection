64d3e368f330f9a0d758ce6b84d2aa53189d05b8
maheshpachpande <pachpandemahesh300@gmail.com>
2025-06-07 17:41:07 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages 
+# setuptools is the most common tool used for packaging Python projects.
+# find_packages() automatically detects all sub-packages in your project by looking for __init__.py files.
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mahesh",
+    author_email="pachpandemahesh300@gmail.com",
+    packages=find_packages()
+)
